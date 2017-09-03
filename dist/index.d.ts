7a77c4793327f0025604dd6f858a7bb115c050b0
Unknown <russleyshaw@gmail.com>
2017-09-03 00:52:30 -0500

    Removed dist

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as db from "./db";
-import * as logger from "./logger";
-import * as middleware from "./middleware";
-import * as node_env from "./node_env";
-export { db, logger, middleware, node_env };
--- dist/middleware/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="express" />
-import { Request, Response, NextFunction } from "express";
-/**
- * Middleware for logging requests
- * @param req
- * @param res
- * @param next
- */
-export declare function logging(req: Request, res: Response, next: NextFunction): void;
