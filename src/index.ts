d688cbc77e538da80a6418ae5d4887e518ec06ca
Unknown <russleyshaw@gmail.com>
2017-09-04 15:21:33 -0500

    Hotfix: import default logger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as db from "./db";
-import * as logger from "./logger";
+import logger from "./logger";
 import * as middleware from "./middleware";
 import * as node_env from "./node_env";
 
