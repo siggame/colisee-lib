82e77da201c1b4ad3280f2b9cb5ac856e930d0ae
Michael Beaver <michaelbeaver64@gmail.com>
2018-03-12 09:50:12 -0500

    Updated unit tests with new table configurations

--- test/unit/db.ts
+++ test/unit/db.ts
@@ -2,6 +2,7 @@ import * as chai from "chai";
 import { expect } from "chai";
 
 import { db } from "../../src";
+import {USER_ROLE, USER_ROLES} from "../../src/db/index";
 
 export default function () {
 
@@ -18,31 +19,25 @@ export default function () {
             it("should map N rows to N teams", async () => {
                 const rows = [
                     {
-                        contact_email: "test@example.com",
-                        contact_name: "test",
                         created_at: Date.now().toString(),
-                        hash_iterations: 0,
                         id: 4,
+                        is_closed: false,
                         is_eligible: true,
-                        name: "yoyo",
-                        password: "password",
-                        role: "admin",
-                        salt: "somesalt",
+                        is_paid: true,
+                        name: "testName",
+                        team_captain_id: 1,
                         updated_at: Date.now().toString(),
                     },
                 ];
                 const teams: db.Team[] = [
                     {
-                        contactEmail: rows[0].contact_email,
-                        contactName: rows[0].contact_name,
                         createdAt: new Date(rows[0].created_at),
-                        hashIterations: rows[0].hash_iterations,
                         id: rows[0].id,
+                        isClosed: rows[0].is_closed,
                         isEligible: rows[0].is_eligible,
+                        isPaid: rows[0].is_paid,
                         name: rows[0].name,
-                        password: rows[0].password,
-                        role: rows[0].role as db.TEAM_ROLE,
-                        salt: rows[0].salt,
+                        teamCaptainId: rows[0].team_captain_id,
                         updatedAt: new Date(rows[0].updated_at),
                     },
                 ];
@@ -52,6 +47,64 @@ export default function () {
             it("should throw if missing element", async () => {
                 try {
                     await db.rowsToTeams([{ invalid: "error" }]);
+                    chai.assert("Expected error");
+                } catch (e) {
+                    expect(e).is.not.instanceOf(chai.AssertionError);
+                }
+            });
+        });
+
+        describe("rowsToUsers", function () {
+            it("should map 0 rows to 0 teams", async () => {
+                expect(db.rowsToTeams([])).to.be.empty;
+            });
+            it("should map N rows to N teams", async () => {
+                const rows = [
+                    {
+                        active: true,
+                        bio: "here is my bio",
+                        contact_email: "test@test.com",
+                        contact_name: "testName",
+                        created_at: Date.now().toString(),
+                        form_response : {
+                            question1: "answer",
+                            question2: "answer2",
+                        },
+                        hash_iterations: 5000,
+                        id: 4,
+                        name: "testName",
+                        password: "testPassword",
+                        profile_pic: "Not sure how to represent a picture",
+                        role: USER_ROLES[0],
+                        salt: "testSalt",
+                        updated_at: Date.now().toString(),
+                    },
+                ];
+                const users: db.User[] = [
+                    {
+                        active: rows[0].active,
+                        bio: rows[0].bio,
+                        contactEmail: rows[0].contact_email,
+                        contactName: rows[0].contact_name,
+                        createdAt: new Date(rows[0].created_at),
+                        formResponse: rows[0].form_response,
+                        hashIterations: rows[0].hash_iterations,
+                        id: rows[0].id,
+                        name: rows[0].contact_name,
+                        password: rows[0].password,
+                        profilePic: rows[0].profile_pic,
+                        role: rows[0].role as USER_ROLE,
+                        salt: rows[0].salt,
+
+                        updatedAt: new Date(rows[0].updated_at),
+                    },
+                ];
+
+                expect(db.rowsToUsers(rows)).to.deep.equals(users);
+            });
+            it("should throw if missing element", async () => {
+                try {
+                    await db.rowsToUsers([{ invalid: "error" }]);
                     chai.assert("Expected error");
                 } catch (e) {
                     expect(e).is.not.instanceOf(chai.AssertionError);
